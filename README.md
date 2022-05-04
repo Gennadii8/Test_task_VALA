a76be9088e2cfafad15e2464e264c1cf61286a0a
Gennadii <Ligr98@gmail.com>
2022-05-04 18:55:02 +0300

    Task done

---
+++ README.md
@@ -0,0 +1,64 @@
+# Test assignment Gennadii Matveev #
+
+## Task #4 - Multiplies of A & B ##
+
+THEORY:  
+Multiples of A and B  
+Let’s assume that A is 4 and B is 7, so
+If we list all the natural numbers below 20 that are multiples of A or B, 
+we get 4, 7, 8, 12, 14 and 16.  
+TASK:
+Create a program that takes a file as the first command line argument. The file content is list of
+numbers, with 3 numbers per row separated by space and number of rows is undefined (can
+be something between 1 - infinite). First number in a row is A and the second is B, third one is
+the goal number (as in theorem 20). The program has to search all multiples of A and B which
+are below the third number, print it out to screen and also write results to file which was given
+as the second command line argument.  
+Program should sort out output file by ascending order how many multiples certain row has.
+Example input file content:  
+5 8 31  
+4 7 20  
+Example command line command: python my_program.py input.txt output.txt
+Example screen output and output file content:  
+20:4 7 8 12 14 16  
+31:5 8 10 15 16 20 24 25 30
+
+## Installation Instructions ##
+No extra libraries needed - requirements.txt is empty.  
+I was using Python 3.10.2, PyCharm on Windows
+
+## Additional info ##
+
+I know how to use parallel opening of two files, but I decided not to use it here 
+so that the code would be more understandable.  
+From the point of view of OOP, I didn't use classes here, I didn't take out individual 
+modules, because it seemed to me that this wasn't important in a small test task and the 
+code is visually and logically understandable.
+
+## What program can do (in addition to the basic functionality) ##
+* Checks that there are exactly 2 arguments of the script (input and output file) 
+* Checks that input and output files have .txt format
+* Checks that input file is not empty
+* Checks that input file exists (I don't check existence of output file, because here I 
+create file with this name or rewrite file with this name)
+* Checks that there are 3 numbers in every line of file
+* Checks that all three elements in row are integers
+In this case I was opening input file, took info out of it, 
+
+## Prepared files ##
+* 666.txt - duplicated 3rd numbers, less than needed numbers, 
+more than needed numbers, letters instead of numbers
+* 555.txt - I was just using it for output (so you can see my last output)
+* 444.txt - empty file
+* 333.tx - wrong type of file
+
+## Prepared use cases ##
+1) python main.py 666.txt 555.txt - will work correctly and list errors
+2) python main.py 444.txt 555.txt - will catch empty file error
+3) python main.py 777.txt 555.txt - will catch non-existing file error
+4) python main.py 333.tx 555.txt - will catch wrong format file error
+5) python main.py 555.txt - will catch that there is only one argument
+
+## Spent time ##
+I did this program in one day with pauses - real working time about 4 hours.  
+I hope that everything is clear. If you have any questions, please email me.
---
+++ main.py
@@ -0,0 +1,134 @@
+import sys
+import os
+from collections import defaultdict
+
+
+def check_number_of_arguments():
+    """Check number of entered arguments and return error to user if there are not 2 of them.
+    Returns list of arguments."""
+
+    number_of_arguments = len(sys.argv) - 1
+    if number_of_arguments != 2:
+        print("Check yours arguments. There should be input.txt and output.txt files.")
+        exit()
+    else:
+        arguments = list(sys.argv[1:])
+        return arguments
+
+
+def check_existence_and_types_of_arguments(entered_arguments):
+    """Takes list that consist of input and output files.
+    Check that entered input file exist, not empty and both entered files have .txt type.
+    Returns error to user if one of checks was failed.
+    Returns list of arguments."""
+
+    # Check that file exist
+    input_file_existence = os.path.isfile(entered_arguments[0])
+    if not input_file_existence:
+        print(f"Impossible to open {entered_arguments[0]}. Please write file's name or it's path correctly.")
+        exit()
+    else:
+
+        # Check that file is not empty
+        if os.stat(entered_arguments[0]).st_size == 0:
+            print(f"{entered_arguments[0]} is empty. Please fill it out.")
+            exit()
+
+        # Check that both files has .txt type
+        for arg in entered_arguments:
+            extension = os.path.splitext(arg)[1]
+            if extension != ".txt":
+                print(f"{arg} is not a .txt file. Input and output files should have .txt type")
+                exit()
+
+    return entered_arguments
+
+
+def make_calculations(input_doc):
+    """Takes input file.
+    Open input file and for every line: check amount of numbers in line (if != 3, copy this line to list of
+    broken lines), check that all symbols in line have integer type (if not copy this line to list of broken lines,
+    create a loop for finding all multiples of both numbers which are below the third number, output is a list of all
+    multiples (with duplicates), Crete a set to remove duplicates and create dict with format that will be comfortable
+    for upcoming sorting (defaultdict) -
+    {number_of_multiples: [[third number, [multiplies from line], [multiplies from another line], ...]]}.
+    Returns - dict for sorting and list of broken lines."""
+
+    # Open file for reading
+    with open(input_doc, "r", encoding="utf-8") as input_file:
+        output_dict = defaultdict(list)
+        list_of_broken_lines = []
+
+        # Going through every line
+        for line in input_file.readlines():
+            list_of_line_symbols = line.split()
+
+            # Check amount of numbers, if != 3, copy this line in list of broken lines
+            if len(list_of_line_symbols) != 3:
+                output_error_0 = f"{line.strip()} - the number of elements in this row is not equal to 3"
+                list_of_broken_lines.append(output_error_0)
+            else:
+                # Check that all numbers are integers, if not copy this line in list of broken lines
+                try:
+                    numbers_list = [int(list_of_line_symbols[0]), int(list_of_line_symbols[1])]
+                    limit = int(list_of_line_symbols[2])
+                except ValueError:
+                    output_error_1 = f"{line.strip()} - some elements in the string are not integers"
+                    list_of_broken_lines.append(output_error_1)
+                    continue
+
+                # Find all multiples of both numbers which are below the third number,
+                # output - list of all multiples (with duplicates)
+                multiples_list = []
+                for number in numbers_list:
+                    i = 1
+                    while i*number < limit:
+                        multiples_list.append(i*number)
+                        i += 1
+
+                # Crete a set to remove duplicates and create dict with format that will be comfortable
+                # for upcoming sorting (defaultdict) -
+                # {number_of_multiples: [[third number, [multiplies from line], [multiplies from another line], ...]]}
+                multiples_set = sorted(set(multiples_list))
+                number_of_multiples = len(multiples_set)
+                one_line_info = [limit, multiples_set]
+                output_dict[number_of_multiples].append(one_line_info)
+
+    return output_dict, list_of_broken_lines
+
+
+def write_calculations_in_file(calculations_dict, broken_str_list, output_doc):
+    """
+    Takes dict for sort, list of broken lines and output file.
+    Doesn't check existence of output file from user - can create it by itself.
+    Open output file, sort the number of multiples and goes through every item of defaultdict, goes through every
+    multiples list (if there are duplicated third numbers) of every third number, creates output line, print it to
+    console and write to output file, print to console and write to file all broken lines.
+    Returns nothing - print output to console and to output file."""
+
+    # Open file for writing (if file already exist, program will rewrite it)
+    with open(output_doc, "w", encoding="utf-8") as output_file:
+
+        # Sort the number of multiples and goes through every item of defaultdict
+        for amount_of_multiples, info_list in sorted(calculations_dict.items()):
+            # Goes through every multiples list (if there are duplicated third numbers) of every third number, creates
+            # output line, print it to console and write to output file
+            for one_info_list in info_list:
+                output_str = ""
+                for one_elem in one_info_list[1]:
+                    output_str += str(one_elem) + " "
+                print(f"{one_info_list[0]}:{output_str}")
+                output_file.write(f"{one_info_list[0]}:{output_str}\n")
+
+        # print to console and write to file all broken lines
+        for broken_str in broken_str_list:
+            output_file.write(f"{broken_str}\n")
+            print(broken_str)
+
+
+if __name__ == "__main__":
+    user_arguments = check_number_of_arguments()
+    correct_arguments = check_existence_and_types_of_arguments(user_arguments)
+    calculated_dict, broken_lines_list = make_calculations(correct_arguments[0])
+    write_calculations_in_file(calculated_dict, broken_lines_list, correct_arguments[1])
+
